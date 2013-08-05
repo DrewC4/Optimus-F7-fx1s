05cce673e3b2b51b5f35edae878699d220d2c317
GameTheory <jrtheory@gmail.com>
2013-08-04 23:01:41 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# LG Optimus F7 (LG870-fx1s)
+
+- Run the following....
+- $ make menuconfig
+- Then go to kernel hacking and change stack frames from 1024 to 1064
+- This allows the adreno drivers to build
---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Aug 4 22:39:53 EDT 2013"
+#define LINUX_COMPILE_BY "tony"
+#define LINUX_COMPILE_HOST "pangolin"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
